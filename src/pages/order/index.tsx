e2f7c69b063fb7d256eedc1a6315736310f07e36
daxiongskr2020 <daxiongskr2020@163.com>
2021-03-07 22:29:11 +0800

    你是谁

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -3,9 +3,7 @@ import  React  from  'react';
 export  default function Order(){
     return  (
         <div>
-             这里是订单
-             这里是新的加工厂
-             加油
+            现在是最新的版本
         </div>
     )
 }
